7f348c151c1fde14b9b0ad80007aa8161a1c82a7
timothy99 <joydisk@gmail.com>
2024-05-12 22:01:38 +0900

    관리자 슬라이드 기능 추가

---
+++ app/Views/csl/slide/list.php
@@ -0,0 +1,124 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>슬라이드</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">홈</a></li>
+                        <li class="breadcrumb-item active">슬라이드</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">목록</h3>
+                            <div class="card-tools">
+                                <div class="input-group input-group-sm">
+                                    <select class="form-control" id="search_condition" name="search_condition">
+                                        <option value="title">제목</option>
+                                        <option value="contents">내용</option>
+                                    </select>
+                                    <input type="text" id="search_text" name="search_text" class="form-control float-right ml-2" placeholder="검색">
+                                    <div class="input-group-append">
+                                    <button type="button" class="btn btn-default" id="search_button" name="search_button">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead class="text-center">
+                                    <tr>
+                                        <th>번호</th>
+                                        <th>정렬순서</th>
+                                        <th>제목</th>
+                                        <th>게시기간</th>
+                                        <th>노출여부</th>
+                                        <th>등록자</th>
+                                        <th>날짜</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+<?php   foreach($list as $no => $val) { ?>
+                                    <tr>
+                                        <td><?=$val->list_no ?></td>
+                                        <td><?=$val->order_no ?></td>
+                                        <td><a href="/csl/slide/view/<?=$val->s_idx ?>"><?=$val->title ?></a></td>
+                                        <td><?=$val->start_date_txt ?> ~ <?=$val->end_date_txt ?></td>
+                                        <td><?=$val->display_yn ?></td>
+                                        <td><?=$val->ins_id ?></td>
+                                        <td><?=$val->ins_date_txt ?></td>
+                                    </tr>
+<?php   } ?>
+<?php   if (count($list) == 0) { ?>
+                                    <tr>
+                                        <td colspan="5" class="text-center">데이터가 없습니다.</td>
+                                    </tr>
+<?php   } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer clearfix">
+<?=$paging_view ?>
+                            <button type="button" class="btn btn-info float-right" id="write" name="write">글쓰기</button>
+                        </div>
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+
+<script>
+    $(window).on("load", function() {
+        // 메뉴강조
+        $("#li-slide-list").addClass("menu-open");
+        $("#upper-slide-list").addClass("active");
+        $("#a-slide-list").addClass("active");
+
+        // 셀렉트 박스 선택
+        $("#search_condition").val("<?=$search_arr["search_condition"] ?>").prop("selected", true);
+        $("#search_text").val("<?=$search_arr["search_text"] ?>");
+    });
+
+    $(function() {
+        $("#search_text").keydown(function(e) {
+            if(e.keyCode == 13) {
+                search();
+            }
+        });
+
+        $("#search_button").click(function(e) {
+            search();
+        });
+
+        $("#write").click(function(e) {
+            location.href = "/csl/slide/write";
+        });
+    });
+
+    function search() {
+        var search_text = $("#search_text").val();
+        var search_condition = $("#search_condition").val();
+        location.href = "/csl/slide/list?page=1&search_text="+search_text+"&search_condition="+search_condition;
+    }
+</script>
---
+++ app/Views/csl/slide/view.php
@@ -0,0 +1,115 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>슬라이드</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">홈</a></li>
+                        <li class="breadcrumb-item active">슬라이드</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">상세보기</h3>
+                        </div><!-- /.card-header -->
+                        <div class="card-body">
+                            <div class="col-12">
+                                <table class="table table-hover table-bordered table-sm">
+                                    <tbody>
+                                        <tr>
+                                            <th style="min-width:150px;max-width:170px;">정렬순서</th>
+                                            <td><?=$info->order_no ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>제목</th>
+                                            <td><?=$info->title ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>내용</th>
+                                            <td><?=$info->contents ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>링크</th>
+                                            <td><?=$info->http_link ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>게시기간</th>
+                                            <td><?=$info->start_date_txt ?> ~ <?=$info->end_date_txt ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>노출여부</th>
+                                            <td><?=$info->display_yn ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>이미지</th>
+                                            <td>
+                                                <img src="/csl/file/view/<?=$info->slide_file ?>" class="img-fluid"><br>
+                                                실제크기 : <?=$info->slide_file_info->image_width ?>px * <?=$info->slide_file_info->image_height ?>px
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>등록자</th>
+                                            <td><?=$info->ins_id ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>등록일</th>
+                                            <td><?=$info->ins_date_txt ?></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <div class="d-flex justify-content-end">
+                                <button type="button" class="btn btn-warning ml-3" id="list" name="list">목록</button>
+                                <button type="button" class="btn btn-danger ml-3" id="delete" name="delete">삭제</button>
+                                <button type="button" class="btn btn-success ml-3" id="edit" name="edit">수정</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+
+<script>
+    $(window).on("load", function() {
+        // 메뉴강조
+        $("#li-slide-list").addClass("menu-open");
+        $("#upper-slide-list").addClass("active");
+        $("#a-slide-list").addClass("active");
+    });
+
+    $(function() {
+        $("#edit").click(function(e) {
+            location.href = "/csl/slide/edit/<?=$info->s_idx ?>";
+        });
+
+        $("#list").click(function(e) {
+            location.href = "/csl/slide/list";
+        });
+
+        $("#delete").click(function(e) {
+            if(confirm("글을 삭제하나요? 삭제하면 복구가 불가능합니다.")) {
+                ajax2("/csl/slide/delete/<?=$info->s_idx ?>");
+            }
+        });
+    });
+
+</script>
