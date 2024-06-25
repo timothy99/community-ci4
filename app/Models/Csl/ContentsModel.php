9b1fe8b6a419f97c95a3a6feb9be7a6dd6135250
timothy99 <joydisk@gmail.com>
2024-06-25 11:08:49 +0900

    콘턴츠 페이징 로직 수정

--- app/Models/Csl/ContentsModel.php
+++ app/Models/Csl/ContentsModel.php
@@ -4,18 +4,21 @@ namespace App\Models\Csl;
 
 use CodeIgniter\Model;
 use Throwable;
+use App\Models\Common\DateModel;
 
 class ContentsModel extends Model
 {
     public function getContentsList($data)
     {
+        $date_model = new DateModel();
+
         $result = true;
         $message = "목록 불러오기가 완료되었습니다.";
 
-        $rows = $data["rows"];
         $page = $data["page"];
 
         $search_arr = $data["search_arr"];
+        $rows = $search_arr["rows"];
         $search_condition = $search_arr["search_condition"];
         $search_text = $search_arr["search_text"];
 
@@ -42,6 +45,9 @@ class ContentsModel extends Model
                 $active_class = "";
             }
             $list[$no]->active_class = $active_class;
+
+            $list[$no]->list_no = $cnt-$no-(($page-1)*$rows);
+            $list[$no]->ins_date_txt = $date_model->convertTextToDate($val->ins_date, 1, 1);
         }
 
         $proc_result = array();
