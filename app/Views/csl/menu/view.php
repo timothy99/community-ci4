516e0810edd5273338b0b71137e60b08d90e02d2
timothy99 <joydisk@gmail.com>
2024-07-29 22:45:04 +0900

    기타 동일한 파일 로딩 오류 수정

--- app/Views/csl/menu/view.php
+++ app/Views/csl/menu/view.php
@@ -38,7 +38,7 @@
                                     <dt class="col-sm-2">첨부파일</dt>
                                     <dd class="col-sm-10">
 <?php   foreach($file_list as $no => $val) { ?>
-                                        <a href="/csl/file/download/<?=$val->file_id ?>"><?=$val->file_name_org ?></a><br>
+                                        <a href="/file/download/<?=$val->file_id ?>"><?=$val->file_name_org ?></a><br>
 <?php   } ?>
                                     </dd>
                                     <dt class="col-sm-2">입력자</dt>
