484ac1ea607d3b3ecbf3a74010e6587556bf7492
timothy99 <joydisk@gmail.com>
2025-03-31 18:31:41 +0900

    단순 alert만 띄우는 기능 추가 #269

--- app/Helpers/alert_helper.php
+++ app/Helpers/alert_helper.php
@@ -4,3 +4,8 @@ function redirect_alert($message, $url)
 {
     echo '<script>alert("'.$message.'"); location.href="'.$url.'" </script>';
 }
+
+function _alert($message)
+{
+    echo '<script>alert("'.$message.'"); </script>';
+}
